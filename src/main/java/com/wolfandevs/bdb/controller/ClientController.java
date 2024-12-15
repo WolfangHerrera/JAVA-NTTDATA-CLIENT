c03ede7db13bfb656962ec0bcce0733751c85f36
wolfangherrera <andreyherrerac@icloud.com>
2024-12-14 22:59:07 -0500

    Add CORS support to ClientController for local development

--- src/main/java/com/wolfandevs/bdb/controller/ClientController.java
+++ src/main/java/com/wolfandevs/bdb/controller/ClientController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,6 +18,7 @@ import com.wolfandevs.bdb.service.ClientRepository;
 
 @RestController
 @RequestMapping("/api/getClient")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ClientController {
 
     @Autowired
